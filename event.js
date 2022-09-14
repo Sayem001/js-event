4ccff1da9479560c211a880f3df4bebb14ec5ec2
Sayem001 <abuu.sayem@gmail.com>
2022-09-14 15:35:39 +0600

    second commit

--- event.js
+++ event.js
@@ -1,3 +1,24 @@
 function makeYellow(){
     document.body.style.backgroundColor='yellow';
 }
+
+
+function makeBlue(){
+    document.body.style.backgroundColor='blue';
+}
+
+const  blueBtn=document.getElementById('make-blue-btn')
+blueBtn.onclick=makeBlue;
+// just function nam likhte hobe 1st braket dewa jabe na na hole agei call kore dibe 
+
+// using annonimus function 
+const greenBtn=document.getElementById('make-green-btn');
+greenBtn.onclick=function(){
+    document.body.style.backgroundColor='green'
+}
+
+// add event listener 
+const goldenRodBtn=document.getElementById('make-goldenRod-btn');
+goldenRodBtn.addEventListener('click',function(){
+    document.body.style.backgroundColor='goldenRod'
+})
